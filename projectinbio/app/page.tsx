62d199a00d023ff5cba1775e1a51e04262bd1a46
pedro2506 <t3pedropaulo@gmail.com>
2025-11-25 10:56:07 -0300

    Fix syntax errors in page.tsx

--- projectinbio/app/page.tsx
+++ projectinbio/app/page.tsx
@@ -1,5 +1,13 @@
 
 export default function Home() {
-  return <div className="bg-background-primary">Hello World</div>;
+  return (
+    <div className="bg-background-primary border h-screen">
+      <Header />
+      <Hero />
+      <VideoExplanation />
+      <Pricing />
+      <FAQ />
+    </div>
+  );
 }
 
